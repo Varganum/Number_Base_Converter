cbfa9aeb7274e2943ae541066a3fd35dbfacdfa9
Varganum <nikolai.sobolev@gmail.com>
2023-01-21 02:27:36 +0600

    Stage 3

--- Number Base Converter/task/src/converter/Main.java
+++ Number Base Converter/task/src/converter/Main.java
@@ -1,123 +1,128 @@
 package converter;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
 
-    private static final String[] MESSAGES = {
-            "Do you want to convert ",
-            " decimal or ",
-            " decimal? (To quit type ",
-            "Enter number in decimal system: ",
-            "Enter target base: ",
+    private static final String[] MESSAGES_STAGE3 = {
+            "Enter two numbers in format: {source base} {target base} (To quit type /exit) ",
+            "Enter number in base ",
+            " to convert to base ",
+            " (To go back type /back) ",
             "Conversion result: ",
-            "Enter source number: ",
-            "Enter source base: ",
-            "Conversion to decimal result: "
     };
 
     private static final String[] COMMAND = {
-            "/from",
-            "/to",
+            "/back",
             "/exit"
     };
 
     public static void main(String[] args) {
         // write your code here
-        String startLine = MESSAGES[0] + COMMAND[0] + MESSAGES[1] + COMMAND[1] + MESSAGES[2] + COMMAND[2] + ") ";
+
         Scanner scanner = new Scanner(System.in);
-        int decimalNumber;
-        int newBase;
+        BigInteger decimalNumber;
+        int sourceBase = 0;
+        int targetBase = 0;
         String sourceNumber;
-        int sourceBase;
         StringBuilder userCommand = new StringBuilder();
-        System.out.print(startLine);
+        String userInput;
+        StringBuilder secondLine = new StringBuilder();
 
         do {
-            userCommand.delete(0, userCommand.length());
-            userCommand.append(scanner.nextLine());
 
-            if (userCommand.toString().equals(COMMAND[0])) {
-                System.out.print(MESSAGES[3]);
-                decimalNumber = scanner.nextInt();
-                System.out.print(MESSAGES[4]);
-                newBase = scanner.nextInt();
-                System.out.println(MESSAGES[5] + convertNumberToBase(decimalNumber, newBase) + "\n");
-                System.out.print(startLine);
-            } else if (userCommand.toString().equals(COMMAND[1])) {
-                System.out.print(MESSAGES[6]);
-                sourceNumber = scanner.nextLine();
-                System.out.print(MESSAGES[7]);
+            System.out.print(MESSAGES_STAGE3[0]);
+            if (scanner.hasNextInt()) {
                 sourceBase = scanner.nextInt();
-                System.out.println(MESSAGES[8] + convertNumberToDecimal(sourceNumber, sourceBase) + "\n");
-                System.out.print(startLine);
+                targetBase = scanner.nextInt();
+            } else {
+                userCommand.setLength(0);
+                userCommand.append(scanner.nextLine());
             }
-        } while (!userCommand.toString().equals(COMMAND[2]));
+            secondLine.setLength(0);
+            secondLine.append(MESSAGES_STAGE3[1]).append(sourceBase).append(MESSAGES_STAGE3[2]).append(targetBase).append(MESSAGES_STAGE3[3]);
 
+            if (sourceBase != 0 && targetBase != 0 && !userCommand.toString().equals(COMMAND[1])) {
+                do {
+                    System.out.print(secondLine);
+                    userCommand.setLength(0);
+
+                    userInput = scanner.nextLine();
+                    while (userInput.isEmpty()) {
+                        userInput = scanner.nextLine();
+                    }
+
+                    userCommand.append(userInput);
+
+                    if (userCommand.toString().equals(COMMAND[0])) {
+                        break;
+                    } else {
+                        sourceNumber = userCommand.toString();
+                    }
+
+                    if (sourceBase == 10) {
+                        decimalNumber = new BigInteger(sourceNumber);
+                        System.out.println(MESSAGES_STAGE3[4] + convertNumberToBase(decimalNumber, targetBase) + "\n");
+                    } else if (sourceBase > 1 && sourceBase < 37 && targetBase > 1 && targetBase < 37) {
+                        decimalNumber = convertNumberToDecimal(sourceNumber, sourceBase);
+                        //System.out.println(decimalNumber);
+                        System.out.println(MESSAGES_STAGE3[4] + convertNumberToBase(decimalNumber, targetBase) + "\n");
+                    }
+
+                } while (!userCommand.toString().equals(COMMAND[0]));
+            }
+        } while (!userCommand.toString().equals(COMMAND[1]));
     }
 
-    private static StringBuilder convertNumberToBase(int decimalNumber, int newBase) {
+    private static StringBuilder convertNumberToBase(BigInteger decimalNumber, int newBase) {
         StringBuilder result = new StringBuilder();
-        int currentQuotient = decimalNumber;
-        int remainder;
+        BigInteger[] quotientAndRemainder = {decimalNumber, BigInteger.ZERO};
         char remainderAsChar;
         if (newBase < 11) {
             do {
-                remainder = currentQuotient % newBase;
-                currentQuotient /= newBase;
-                result.insert(0, remainder);
-            } while (currentQuotient != 0);
-        } else if (newBase < 17) {
+                quotientAndRemainder = quotientAndRemainder[0].divideAndRemainder(BigInteger.valueOf(newBase));
+                result.insert(0, quotientAndRemainder[1]);
+            } while (!quotientAndRemainder[0].equals(BigInteger.ZERO));
+        } else if (newBase < 37) {
             do {
-                remainder = currentQuotient % newBase;
-                remainderAsChar = convertRemainder(remainder);
-                currentQuotient /= newBase;
+                quotientAndRemainder = quotientAndRemainder[0].divideAndRemainder(BigInteger.valueOf(newBase));
+                remainderAsChar = convertRemainder(quotientAndRemainder[1].intValue());
                 result.insert(0, remainderAsChar);
-            } while (currentQuotient != 0);
+            } while (!quotientAndRemainder[0].equals(BigInteger.ZERO));
         }
         return result;
     }
 
-    private static int convertNumberToDecimal(String sourceNumber, int sourceBase) {
-        int multiplier = 1;
+    private static BigInteger convertNumberToDecimal(String sourceNumber, int sourceBase) {
+        BigInteger multiplier = BigInteger.ONE;
         char[] sourceDigits = sourceNumber.toCharArray();
-        int result = multiplier * convertCharDigitToDecimal(sourceDigits[sourceDigits.length - 1]);
+        BigInteger result = multiplier.multiply(BigInteger.valueOf(convertCharDigitToDecimal(sourceDigits[sourceDigits.length - 1])));
         for (int i = sourceDigits.length - 2; i >= 0; i--) {
-            multiplier *= sourceBase;
-            result += convertCharDigitToDecimal(sourceDigits[i]) * multiplier;
+            multiplier = multiplier.multiply(BigInteger.valueOf(sourceBase));
+            result = result.add(multiplier.multiply(BigInteger.valueOf(convertCharDigitToDecimal(sourceDigits[i]))));
         }
         return result;
     }
 
     private static char convertRemainder(int remainder) {
-        switch (remainder) {
-            case 0 -> {return '0';}
-            case 1 -> {return '1';}
-            case 2 -> {return '2';}
-            case 3 -> {return '3';}
-            case 4 -> {return '4';}
-            case 5 -> {return '5';}
-            case 6 -> {return '6';}
-            case 7 -> {return '7';}
-            case 8 -> {return '8';}
-            case 9 -> {return '9';}
-            case 10 -> {return 'A';}
-            case 11 -> {return 'B';}
-            case 12 -> {return 'C';}
-            case 13 -> {return 'D';}
-            case 14 -> {return 'E';}
-            case 15 -> {return 'F';}
-            default -> {return 'X';}
+        char result = '@';
+        if (remainder < 10) {
+            result = (char) ('0' + remainder);
+        } else if (remainder < 37) {
+            result = (char) (remainder - 10 + 'A');
         }
+
+        return result;
     }
 
     private static int convertCharDigitToDecimal(char charDigit) {
         int result = 0;
         if (charDigit >= '0' && charDigit <= '9') {
             result = charDigit - '0';
-        } else if (charDigit >= 'A' && charDigit <= 'F') {
+        } else if (charDigit >= 'A' && charDigit <= 'Z') {
             result = charDigit - 'A' + 10;
-        } else if (charDigit >= 'a' && charDigit <= 'f') {
+        } else if (charDigit >= 'a' && charDigit <= 'z') {
             result = charDigit - 'a' + 10;
         }
         return result;
