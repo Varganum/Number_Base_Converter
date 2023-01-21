34bd6f6517bcd44e29566d01b63263201cb87c33
Varganum <nikolai.sobolev@gmail.com>
2023-01-22 00:51:31 +0600

    Final 4th stage of learning project Number Base Converter.

--- Number Base Converter/task/src/converter/Main.java
+++ Number Base Converter/task/src/converter/Main.java
@@ -1,9 +1,13 @@
 package converter;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class Main {
+
+    private static final int OUTPUT_SCALE = 5;
 
     private static final String[] MESSAGES_STAGE3 = {
             "Enter two numbers in format: {source base} {target base} (To quit type /exit) ",
@@ -22,7 +26,7 @@ public class Main {
         // write your code here
 
         Scanner scanner = new Scanner(System.in);
-        BigInteger decimalNumber;
+        BigDecimal decimalNumber;
         int sourceBase = 0;
         int targetBase = 0;
         String sourceNumber;
@@ -62,12 +66,16 @@ public class Main {
                     }
 
                     if (sourceBase == 10) {
-                        decimalNumber = new BigInteger(sourceNumber);
+                        decimalNumber = new BigDecimal(sourceNumber);
                         System.out.println(MESSAGES_STAGE3[4] + convertNumberToBase(decimalNumber, targetBase) + "\n");
                     } else if (sourceBase > 1 && sourceBase < 37 && targetBase > 1 && targetBase < 37) {
                         decimalNumber = convertNumberToDecimal(sourceNumber, sourceBase);
                         //System.out.println(decimalNumber);
-                        System.out.println(MESSAGES_STAGE3[4] + convertNumberToBase(decimalNumber, targetBase) + "\n");
+                        if (sourceNumber.contains(".")) {
+                            System.out.println(MESSAGES_STAGE3[4] + convertNumberToBase(decimalNumber, targetBase) + "\n");
+                        } else {
+                            System.out.println(MESSAGES_STAGE3[4] + convertNumberToBase(decimalNumber.setScale(0, RoundingMode.CEILING), targetBase) + "\n");
+                        }
                     }
 
                 } while (!userCommand.toString().equals(COMMAND[0]));
@@ -75,34 +83,100 @@ public class Main {
         } while (!userCommand.toString().equals(COMMAND[1]));
     }
 
-    private static StringBuilder convertNumberToBase(BigInteger decimalNumber, int newBase) {
-        StringBuilder result = new StringBuilder();
-        BigInteger[] quotientAndRemainder = {decimalNumber, BigInteger.ZERO};
+    private static StringBuilder convertNumberToBase(BigDecimal decimalNumber, int newBase) {
+        StringBuilder resultIntegerPart = new StringBuilder();
+        StringBuilder resultFractionPart = new StringBuilder();
+        int currentFractionDigit;
+        int scaleCounter = 0;
+
+        BigInteger decimalIntegerPart = decimalNumber.toBigInteger();
+        BigDecimal decimalIntegerPartAsBigDecimal = new BigDecimal(decimalIntegerPart);
+        BigDecimal decimalFractionPart = decimalNumber.subtract(decimalIntegerPartAsBigDecimal);
+        BigDecimal currentProduct;
+        BigDecimal currentFractionRemainder;
+
+        BigInteger[] quotientAndRemainder = {decimalIntegerPart, BigInteger.ZERO};
         char remainderAsChar;
+        char currentFractionDigitAsChar;
+
         if (newBase < 11) {
             do {
                 quotientAndRemainder = quotientAndRemainder[0].divideAndRemainder(BigInteger.valueOf(newBase));
-                result.insert(0, quotientAndRemainder[1]);
+                resultIntegerPart.insert(0, quotientAndRemainder[1]);
             } while (!quotientAndRemainder[0].equals(BigInteger.ZERO));
         } else if (newBase < 37) {
             do {
                 quotientAndRemainder = quotientAndRemainder[0].divideAndRemainder(BigInteger.valueOf(newBase));
                 remainderAsChar = convertRemainder(quotientAndRemainder[1].intValue());
-                result.insert(0, remainderAsChar);
+                resultIntegerPart.insert(0, remainderAsChar);
             } while (!quotientAndRemainder[0].equals(BigInteger.ZERO));
         }
+
+        currentFractionRemainder = decimalFractionPart;
+        if (newBase < 11) {
+            do {
+                currentProduct = currentFractionRemainder.multiply(BigDecimal.valueOf(newBase));
+                currentFractionDigit = currentProduct.intValue();
+                currentFractionRemainder = currentProduct.subtract(BigDecimal.valueOf(currentFractionDigit));
+                resultFractionPart.append(currentFractionDigit);
+                scaleCounter++;
+            } while (scaleCounter < OUTPUT_SCALE);
+        } else if (newBase < 37) {
+            do {
+                currentProduct = currentFractionRemainder.multiply(BigDecimal.valueOf(newBase));
+                currentFractionDigit = currentProduct.intValue();
+                currentFractionRemainder = currentProduct.subtract(BigDecimal.valueOf(currentFractionDigit));
+                currentFractionDigitAsChar = convertRemainder(currentFractionDigit);
+                resultFractionPart.append(currentFractionDigitAsChar);
+                scaleCounter++;
+            } while (scaleCounter < OUTPUT_SCALE);
+        }
+
+        StringBuilder result = resultIntegerPart;
+        if (!decimalFractionPart.equals(BigDecimal.ZERO)) {
+            result.append('.').append(resultFractionPart);
+        }
+
         return result;
     }
 
-    private static BigInteger convertNumberToDecimal(String sourceNumber, int sourceBase) {
-        BigInteger multiplier = BigInteger.ONE;
-        char[] sourceDigits = sourceNumber.toCharArray();
-        BigInteger result = multiplier.multiply(BigInteger.valueOf(convertCharDigitToDecimal(sourceDigits[sourceDigits.length - 1])));
-        for (int i = sourceDigits.length - 2; i >= 0; i--) {
-            multiplier = multiplier.multiply(BigInteger.valueOf(sourceBase));
-            result = result.add(multiplier.multiply(BigInteger.valueOf(convertCharDigitToDecimal(sourceDigits[i]))));
+
+    private static BigDecimal convertNumberToDecimal(String sourceNumberAsString, int sourceBase) {
+
+        char[] sourceIntegerPartDigits;
+        char[] sourceFractionPart = null;
+
+        if (sourceNumberAsString.contains(".")) {
+            String[] numberParts = sourceNumberAsString.split("\\.");
+            sourceIntegerPartDigits = numberParts[0].toCharArray();
+            sourceFractionPart = numberParts[1].toCharArray();
+        } else {
+            sourceIntegerPartDigits = sourceNumberAsString.toCharArray();
         }
-        return result;
+
+
+        BigDecimal multiplier = BigDecimal.ONE;
+        BigDecimal resultIntegerPart = multiplier.multiply(BigDecimal.valueOf(convertCharDigitToDecimal(sourceIntegerPartDigits[sourceIntegerPartDigits.length - 1])));
+        for (int i = sourceIntegerPartDigits.length - 2; i >= 0; i--) {
+            multiplier = multiplier.multiply(BigDecimal.valueOf(sourceBase));
+            resultIntegerPart = resultIntegerPart.add(multiplier.multiply(BigDecimal.valueOf(convertCharDigitToDecimal(sourceIntegerPartDigits[i]))));
+        }
+
+        BigDecimal resultFractionPart = new BigDecimal(0);
+        resultFractionPart.setScale(10, RoundingMode.CEILING);
+        int fractionDigitsCounter = 0;
+        if (sourceFractionPart != null) {
+            int currentDigit;
+            BigDecimal divisor = new BigDecimal(sourceBase);
+            while (fractionDigitsCounter < sourceFractionPart.length) {
+                currentDigit = convertCharDigitToDecimal(sourceFractionPart[fractionDigitsCounter]);
+                resultFractionPart = resultFractionPart.add(BigDecimal.valueOf(currentDigit).divide(divisor, 10, RoundingMode.CEILING));
+                divisor = divisor.multiply(BigDecimal.valueOf(sourceBase));
+                fractionDigitsCounter++;
+            }
+        }
+
+        return resultIntegerPart.add(resultFractionPart).setScale(10, RoundingMode.CEILING);
     }
 
     private static char convertRemainder(int remainder) {
